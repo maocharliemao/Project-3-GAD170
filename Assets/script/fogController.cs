a7d4889d4ec366a82a538bbce269c201addb2a35
DINO-DINO\User <maocharliemao@gmail.com>
2022-11-27 20:05:51 +1100

    inital commit

---
+++ Assets/script/fogController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fogController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "player")
+        {
+            other.gameObject.GetComponent<PlayerStats>().poisoned = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "player")
+        {
+            other.gameObject.GetComponent<PlayerStats>().poisoned = false;
+        }
+    }
+}
